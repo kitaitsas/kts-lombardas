c07c808ef035890b6fbbc08788714af7bd9ddd94
! Kitaitsas <rokaspetraitis2023@gmail.com>
2025-12-29 18:22:43 +0200

    Rename event callbacks for lombardas functionality

--- server.lua
+++ server.lua
@@ -1,6 +1,6 @@
 ESX = exports['es_extended']:getSharedObject()
 
-ESX.RegisterServerCallback('redt-lombardas:canAccess', function(source, cb)
+ESX.RegisterServerCallback('kts-lombardas:canAccess', function(source, cb)
     local players = ESX.GetPlayers()
     for i = 1, #players do
         local xPlayer = ESX.GetPlayerFromId(players[i])
@@ -12,7 +12,7 @@ ESX.RegisterServerCallback('redt-lombardas:canAccess', function(source, cb)
     cb(true)
 end)
 
-RegisterNetEvent('redt-lombardas:buyItemWithMethod', function(item, price, method)
+RegisterNetEvent('kts-lombardas:buyItemWithMethod', function(item, price, method)
     local src = source
     local xPlayer = ESX.GetPlayerFromId(src)
 
@@ -20,18 +20,19 @@ RegisterNetEvent('redt-lombardas:buyItemWithMethod', function(item, price, metho
         if xPlayer.getMoney() >= price then
             xPlayer.removeMoney(price)
         else
-            TriggerClientEvent('redt-lombardas:showNotify', src, 'Klaida', 'Nepakanka grynųjų', 'error')
+            TriggerClientEvent('kts-lombardas:showNotify', src, 'Klaida', 'Nepakanka grynųjų', 'error')
             return
         end
     elseif method == 'bank' then
         if xPlayer.getAccount('bank').money >= price then
             xPlayer.removeAccountMoney('bank', price)
         else
-            TriggerClientEvent('redt-lombardas:showNotify', src, 'Klaida', 'Nepakanka pinigų banke', 'error')
+            TriggerClientEvent('kts-lombardas:showNotify', src, 'Klaida', 'Nepakanka pinigų banke', 'error')
             return
         end
     end
 
     xPlayer.addInventoryItem(item, 1)
-    TriggerClientEvent('redt-lombardas:showNotify', src, 'Sėkmingai', 'Prekė nusipirkta sėkmingai!', 'success')
+    TriggerClientEvent('kts-lombardas:showNotify', src, 'Sėkmingai', 'Prekė nusipirkta sėkmingai!', 'success')
+
 end)
